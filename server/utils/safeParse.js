488407274fad77f106a17e77f946a614265e6276
dbitton1978-afk <dbitton1978@gmail.com>
2026-03-26 06:45:51 +0200

    Create safeParse.js

---
+++ server/utils/safeParse.js
@@ -0,0 +1,11 @@
+export function safeJsonParse(text) {
+  try {
+    return JSON.parse(text);
+  } catch {
+    try {
+      const match = text.match(/\{[\s\S]*\}/);
+      if (match) return JSON.parse(match[0]);
+    } catch {}
+    return {};
+  }
+}
