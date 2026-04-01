0544215e0f09a151eb3aa43928066361444726c1
dbitton1978-afk <dbitton1978@gmail.com>
2026-04-01 09:46:42 +0300

    Update Post.js

--- server/models/Post.js
+++ server/models/Post.js
@@ -3,8 +3,7 @@ import mongoose from "mongoose";
 const postSchema = new mongoose.Schema(
   {
     userId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
+      type: String,
       required: true,
       index: true
     },
@@ -15,7 +14,8 @@ const postSchema = new mongoose.Schema(
     },
     content: {
       type: mongoose.Schema.Types.Mixed,
-      required: true
+      required: true,
+      default: {}
     }
   },
   {
