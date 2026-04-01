5f88dd862b29a483942f1c232b90d7126f794101
dbitton1978-afk <dbitton1978@gmail.com>
2026-04-01 13:49:23 +0300

    Update Post.js

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,20 +1,30 @@
 import mongoose from "mongoose";
 
-const postSchema = new mongoose.Schema(
+const PostSchema = new mongoose.Schema(
   {
-    userId: {
-      type: String,
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
       required: true,
       index: true
     },
     type: {
       type: String,
       enum: ["build", "improve", "analyze"],
-      default: "build"
-    },
-    content: {
-      type: mongoose.Schema.Types.Mixed,
       required: true,
+      index: true
+    },
+    language: {
+      type: String,
+      enum: ["he", "en"],
+      default: "he"
+    },
+    input: {
+      type: mongoose.Schema.Types.Mixed,
+      default: {}
+    },
+    data: {
+      type: mongoose.Schema.Types.Mixed,
       default: {}
     }
   },
@@ -23,4 +33,4 @@ const postSchema = new mongoose.Schema(
   }
 );
 
-export default mongoose.model("Post", postSchema);
+export default mongoose.models.Post || mongoose.model("Post", PostSchema);
