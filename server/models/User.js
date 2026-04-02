65e837ec8e6eae70e28e5e13eec370db1c14684b
dbitton1978-afk <dbitton1978@gmail.com>
2026-04-02 07:48:07 +0300

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -2,21 +2,10 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true,
-      lowercase: true
-    },
-    password: {
-      type: String,
-      required: true
-    }
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
   },
-  {
-    timestamps: true
-  }
+  { timestamps: true }
 );
 
 export default mongoose.model("User", userSchema);
